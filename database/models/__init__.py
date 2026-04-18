6f4a78298252c1e8d62f82f0811c400fa32da9aa
d0kuzu <dkobdabaev@mail.ru>
2026-04-18 23:58:53 +0500

    hot fix

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -2,6 +2,10 @@ from .barcode import Barcode
 from .base import Base
 from .user import User
 from .ban import Ban
+from .profile import Profile
+from .action import Action
+from .complaint import Complaint
+from .notification import Notification
 
 
-__all__ = ["Base", "User", "Barcode", "Ban"]
+__all__ = ["Base", "User", "Barcode", "Ban", "Profile", "Action", "Complaint", "Notification"]
