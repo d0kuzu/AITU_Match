e25cb69a6a9308b3e6c87ac75f4db5ea89bd3c80
d0kuzu <dkobdabaev@mail.ru>
2026-06-28 15:34:39 +0500

    fix: refresher fix

--- telegram/jobs/actions_refresher.py
+++ telegram/jobs/actions_refresher.py
@@ -3,15 +3,15 @@ from datetime import datetime, timedelta
 from config.enums import ActionEnum
 from database.repo import Repos
 from database.session import get_db
+import logging
 
 
 async def actions_refresher():
+    logging.info("refresher started")
     async with get_db() as session:
         repos = Repos(session)
 
         actions = await repos.action.get_all_actions()
         for action in actions:
-            if action.action_type == ActionEnum.skip and action.created_at <= datetime.now() - timedelta(days=10):
-                await repos.action.delete_action(action.id)
-            if action.action_type == ActionEnum.like and action.created_at <= datetime.now() - timedelta(days=15):
+            if action.created_at <= datetime.now() - timedelta(days=10):
                 await repos.action.delete_action(action.id)
